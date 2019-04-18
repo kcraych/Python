19963003fed3b7ff375de358810529af27d3c923
kcraych@gmail.com <kcraych@gmail.com>
2019-04-18 17:43:00 -0600

    Add recursion class or Koch Curve

--- Skills-Practice/Fractals/Koch Curve.py
+++ Skills-Practice/Fractals/Koch Curve.py
@@ -6,67 +6,61 @@ screen_w = 1024
 screen_h = 768
 screen = pygame.display.set_mode((screen_w, screen_h))
 
-class Matrix_Utility:
-    @Staticmethod
-    # returns points of a non-origin coordinate after a counter-clockwise rotation about the origin of angle (in radians)
-    def rot_cc_origin(angle, start_x = 1, start_y = 0):
+import numpy as np
+
+class Utility_Matrix:
+    @staticmethod
+    # returns new coordinates after a counter-clockwise rotation about the origin of angle (in radians)
+    # coordinates parameter = 2 x n array where each column represents an x,y pair to rotated
+    def rot_cc_origin(angle, coordinates):
         matrix_rot = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-        matrix_pos = np.array([[start_x],[start_y]])
-        return np.matmul(matrix_rot, matrix_pos)
+        return np.matmul(matrix_rot, coordinates)
 
-    @Staticmethod
-    # returns points of a coordinate after a horizontal and/or vertical translation
-    def translation(delta_x, delta_y, start_x = 0, start_y = 0):
-        return np.array([[start_x], [start_y]]) + np.array([[delta_x], [delta_y]])
-
-    @Staticmethod
-    def full_transformation(angle, delta_x, delta_y, start_x = 1, start_y = 0, scale = 1):
-        result_rotate = Matrix_Utility.rot_cc_origin(angle, start_x, start_y)
+    @staticmethod
+    # returns new coordinates after a scale, rotation, and translation (in that order)
+    # coordinates parameter = 2 x n array where each column represents an x,y pair to rotated
+    def full_transformation(angle, delta_x, delta_y, coordinates, scale = 1):
+        result_rotate = Utility_Matrix.rot_cc_origin(angle, coordinates)
         result_scale = scale * result_rotate
-        result_translate = Matrix_Utility.translation(delta_x, delta_y, result_scale[0,0], result_scale[0,1])
+        result_translate = result_scale + np.array([[delta_x]*coordinates.shape[1],[delta_y]*coordinates.shape[1]])
         return result_translate
 
-class Polygon_Utility:
-    @Staticmethod
-    def reg_poly_angles_radians(n):
-        return [np.deg2rad(360/n), np.deg2rad(180-(360/n))]  # [inner angle, vertex angle]
+class Utility_Polygon:
+    @staticmethod
+    def reg_poly_inner_angle_radians(n):
+        return np.deg2rad(360/n)
 
-    @Staticmethod
-    def reg_poly_identity_coordinates_origin(n):
-        poly_angles = Polygon_Utility.reg_poly_angles_radians(n)
-        poly_vertices_x = [-np.cos(poly_angles[1] / 2)]
-        poly_vertices_y = [-np.sin(poly_angles[1] / 2)]
-        for i in range (1, n-1):
-            poly_vertices_new = Matrix_Utility.rot_cc_origin(poly_angles[0], poly_vertices_x[i-1], poly_vertices_y[i-1])
-            poly_vertices_x.append(poly_vertices_new[0,0])
-            poly_vertices_y.append(poly_vertices_new[1,0])
+    @staticmethod
+    # identifies all coordinates of a regular polygon of side length, with the lower left vertex on the start_x, start_y position
+    def reg_poly_coordinates_identity_origin(n, side_length = 1, start_x = 0, start_y = 0):
+        poly_angle = Utility_Polygon.reg_poly_inner_angle_radians(n)
+        poly_vertices = np.array([[-0.5*side_length], [-0.5*side_length*np.cos(0.5*poly_angle)/np.sin(0.5*poly_angle)]])
+        for i in range (n-1):
+            new_vertices = Utility_Matrix.rot_cc_origin(-poly_angle, poly_vertices[:,[i]])
+            poly_vertices = np.concatenate((poly_vertices, new_vertices), axis=1)
+        poly_vertices += np.array([[-poly_vertices[0,0] + start_x]*n, [-poly_vertices[1,0] + start_y]*n])
+        return poly_vertices
 
-    @Staticmethod
-    def reg_poly_coordinates(n, radius, x, y):
-        poly_angles = Polygon_Utility.reg_poly_angles_radians(n)
+class Utility_Fractals:
+    def unit_koch_curve(n = 3, start_x = 0, start_y = 0, theta = 0, unit_length = 1, poly_ratio = 1/3):
+        unit_poly_coordinates = Utility_Polygon.reg_poly_coordinates_identity_origin(n, poly_ratio, 0.5*(1-poly_ratio), 0)
+        unit_koch_coordinates = np.concatenate((np.array([[0],[0]]), unit_poly_coordinates, np.array([[1],[0]])), axis=1)
+        koch_coordinates = Utility_Matrix.full_transformation(theta, start_x, start_y, unit_koch_coordinates, unit_length)
+        return koch_coordinates
 
-
-
-
-
-
-class Fractals_Koch_Curve
-    def __init__(self, order, ratio, size, n, pos):
-        ang = Fractals_Utility.reg_poly_vertex_angle_radians(n)
-
-
-class Fractals_Koch_Curve_Unit
-    def __init__(self, ratio, size, angle, pos, base_angle):
-        rotations = [0, angle, -angle, 0]
-        for rot in rotations:
-            pos_arr = np.array([[pos.x], [pos.y]])
-            newpos_arr = matrix_manipulation(this, this.ratio, this.pos_arr, rot, 0)
-            newpos = (newpos_arr[0, 0], newpos_arr[1, 0])
-            pygame.draw.line(screen, (0, 0, 255), pos, newpos)
-            pos = newpos
-
-
-
+class Fractal_Generation:
+    def koch_curve(n, start_x, start_y, theta, unit_length, poly_ratio):
+        unit_coordinates = Utility_Fractals.unit_koch_curve(n, start_x, start_y, theta, unit_length, poly_ratio)
+        koch_coordinates = unit_coordinates[:,[0]]
+        for j in range(1,unit_coordinates.shape[1]-2):
+            start_x = unit_coordinates[0,j]
+            start_y = unit_coordinates[1,j]
+            coordinates_delta = unit_coordinates[:,[j+1]] - unit_coordinates[:,[j]]
+            theta = np.arctan(coordinates_delta[1,0]/coordinates_delta[0,0])
+            unit_new = Utility_Fractals.unit_koch_curve(n, start_x, start_y, theta, unit_length, poly_ratio)
+            koch_coordinates = np.concatenate((koch_coordinates, unit_new[:,:(n+1)]), axis=1)
+        koch_coordinates = np.concatenate((koch_coordinates, unit_coordinates[:,[n+1]]), axis=1)
+        return koch_coordinates
 
 def draw_loop():
     while True:
