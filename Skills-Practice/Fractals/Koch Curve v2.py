442e5385ced2a2dbe739ffa35133b849630541b1
kcraych@gmail.com <kcraych@gmail.com>
2019-04-24 18:15:51 -0600

    Add pattern recursion breakout

--- Skills-Practice/Fractals/Koch Curve v2.py
+++ Skills-Practice/Fractals/Koch Curve v2.py
@@ -6,19 +6,37 @@ screen_w = 1024
 screen_h = 768
 screen = pygame.display.set_mode((screen_w, screen_h))
 
-class Utility_Matrix:
+class Utility_Methods:
     @staticmethod
     # returns new coordinates after a scale, rotation, and translation (in that order)
     # coordinates parameter = 2 x n array where each column represents an x,y pair to rotated
-    def matrix_transformation(angle, delta_x, delta_y, coordinates, scale=1):
+    def matrix_transformation(coordinates, angle=0, delta_x=0, delta_y=0, scale=1):
         matrix_rotate = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
         result_rotate = np.matmul(matrix_rotate, coordinates)
         result_scale = scale * result_rotate
         result_translate = result_scale + np.array([[delta_x] * coordinates.shape[1], [delta_y] * coordinates.shape[1]])
         return result_translate
 
+    @staticmethod
+    # returns the coordinates from taking a pattern given by a set of coordinates starting at the origin and repeating
+    # the pattern after scaling to various lengths and rotating at various angles, always connecting the next scale and
+    # rotation at the final point of the previous scaled and rotation.
+    def pattern_repeats(a, s, p=np.array([[0, 1], [0, 0]])):
+        repeats = np.array([[0, 0]]).T
+        for i in range(a.size, 0, -1):
+            section = Utility_Methods.matrix_transformation(p, a[i - 1], repeats[0, -1], repeats[1, -1], s[i - 1])
+            repeats = np.concatenate((repeats, np.delete(section, 0, axis=1)), axis=1)
+        return repeats
 
-class Utility_Polygon:
+    @staticmethod
+    # returns the result of recursively calling a pattern repeat
+    def pattern_recursion(order, n, angles, scales, pattern=np.array([[0, 1], [0, 0]])):
+        if order == 0:
+            return pattern
+        else:
+            new_pattern = Utility_Methods.pattern_repeats(angles, scales, pattern)
+            return Utility_Methods.pattern_recursion(order-1, n, angles, scales, new_pattern)
+
     @staticmethod
     def reg_poly_vertex_radians(n):
         return np.deg2rad(180 - 360 / n)
@@ -29,16 +47,18 @@ class Utility_Polygon:
 
 
 class Fractal_Generation:
-    def koch_curve_recursion(order, n, pos, theta, scales, angles, inserts):
-        if order == 0:
-            return Utility_Matrix.matrix_transformation(theta, pos[0], pos[1], inserts)
-        else:
-            curve = np.array([[0, 0]]).T
-            for i in range(n + 1, 0, -1):
-                inserts_transform = Utility_Matrix.matrix_transformation(angles[i - 1], curve[0, -1], curve[1, -1], inserts, scales[i - 1])
-                curve = np.concatenate((curve, np.delete(inserts_transform, 0, axis=1)), axis=1)
-            return Fractal_Generation.koch_curve_recursion(order-1, n, pos, theta, scales, angles, curve)
+    def koch_curve(order, n, ratio):
+        v_rad = Utility_Methods.reg_poly_vertex_radians(n)
+        i_rad = Utility_Methods.reg_poly_inner_radians(n)
+        angles = np.insert([0., 0.], 1, np.array([v_rad] * (n - 1)) - np.array(range(0, n - 1)) * i_rad)
+        scales = np.insert([(1 - ratio) / 2] * 2, 1, [ratio] * (n - 1))
+        return Utility_Methods.pattern_recursion(order, n, angles, scales)
 
+    def reg_poly_pattern(n):
+        v_rad = Utility_Methods.reg_poly_vertex_radians(n)
+        i_rad = Utility_Methods.reg_poly_inner_radians(n)
+        angles = np.insert([0., 0.], 1, np.array([v_rad] * (n - 1)) - np.array(range(0, n - 1)) * i_rad)
+        return Utility_Methods.pattern_recursion(1, n, [1]*n, angles)
 
 def draw_loop(order, n, pos, length, ratio, theta):
     while True:
@@ -49,16 +69,13 @@ def draw_loop(order, n, pos, length, ratio, theta):
             if event.key == pygame.K_ESCAPE:
                 break;
 
-        v_rad = Utility_Polygon.reg_poly_vertex_radians(n)
-        i_rad = Utility_Polygon.reg_poly_inner_radians(n)
-        angles = np.insert([0.,0.],1,np.array([v_rad]*(n-1))-np.array(range(0,n-1))*i_rad)
-        scales = np.insert([(1 - ratio) / 2] * 2, 1, [ratio] * (n - 1))
-        inserts = np.array([[0, length], [0, 0]])
-        curve = np.transpose(Fractal_Generation.koch_curve_recursion(order, n, pos, theta, scales, angles, inserts))
+        curve = Fractal_Generation.koch_curve(order, n, ratio)
+        curve = Utility_Methods.matrix_transformation(curve, theta, pos[0], pos[1], length)
+        print(curve.T)
 
-        pygame.draw.lines(screen, (0, 0, 255), False, curve, 1)
+        pygame.draw.lines(screen, (0, 0, 255), False, curve.T, 1)
         pygame.display.flip()
 
-
-draw_loop(5, 3, [100, 700], 800, 1 / 3, np.deg2rad(-35))
+draw_loop(3, 3, [100,700], 800, 1/3, 0)
 pygame.quit()
+
